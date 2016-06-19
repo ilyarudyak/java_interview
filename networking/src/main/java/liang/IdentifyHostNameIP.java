897cd47e73f4832a3254ba94547e5ff7df454959
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-06-19 18:04:32 +0300

    add demo InetAddress

---
+++ networking/src/main/java/liang/IdentifyHostNameIP.java
@@ -0,0 +1,23 @@
+package liang;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by ilyarudyak on 6/19/16.
+ */
+public class IdentifyHostNameIP {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            try {
+                InetAddress address = InetAddress.getByName(args[i]);
+                System.out.print("Host name: " + address.getHostName() + " ");
+                System.out.println("IP address: " + address.getHostAddress());
+            }
+            catch (UnknownHostException ex) {
+                System.err.println("Unknown host or IP address " + args[i]);
+            }
+        }
+    }
+}
