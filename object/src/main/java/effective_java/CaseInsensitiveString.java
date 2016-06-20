e6f6e95946803e9acacce8fa3db6cca3443a9306
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-06-20 11:18:43 +0300

    add examples on wrong overriding of equal and hashcode

---
+++ object/src/main/java/effective_java/CaseInsensitiveString.java
@@ -0,0 +1,58 @@
+package effective_java;
+
+/**
+ * Created by ilyarudyak on 6/19/16.
+ */
+public class CaseInsensitiveString {
+
+    private final String s;
+
+    public CaseInsensitiveString(String s) {
+        if (s == null)
+            throw new NullPointerException();
+        this.s = s;
+    }
+
+    // Broken - violates symmetry!
+//    @Override
+//    public boolean equals(Object o) {
+//
+//        if (o instanceof CaseInsensitiveString) {
+//
+//            CaseInsensitiveString cisLocal = (CaseInsensitiveString) o;
+//            System.out.print("o=" + o + " cisLocal=" + cisLocal + " " + s + " ");
+//            System.out.println("s.equalsIgnoreCase(cisLocal.s)=" + s.equalsIgnoreCase( cisLocal.s ));
+//
+//            return s.equalsIgnoreCase( cisLocal.s );
+//        }
+//        if (o instanceof String) {// One-way interoperability!
+//
+//            String oCast = (String) o;
+//            System.out.print("o=" + o + " oCast=" + oCast + " s=" + s + " ");
+//            System.out.println("s.equalsIgnoreCase(oCast)=" + s.equalsIgnoreCase( oCast ));
+//            return s.equalsIgnoreCase( oCast );
+//        }
+//        return false;
+//    }
+
+    @Override
+    public String toString() {
+        return s;
+    }
+
+    // This version is correct - no comparison with String at all
+     @Override
+     public boolean equals(Object o) {
+     return o instanceof CaseInsensitiveString &&
+     ((CaseInsensitiveString) o).s.equalsIgnoreCase(s);
+     }
+
+    public static void main(String[] args) {
+        CaseInsensitiveString cis = new CaseInsensitiveString("Polish");
+        CaseInsensitiveString cis2 = new CaseInsensitiveString("pOLISH");
+        String str = "polish";
+
+        // true false
+        System.out.println(cis.equals(str) + "  " + cis.equals(cis2) + " " + str.equals(cis));
+    }
+}
---
+++ object/src/main/java/effective_java/PhoneNumber.java
@@ -0,0 +1,81 @@
+package effective_java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ilyarudyak on 6/20/16.
+ */
+public class PhoneNumber {
+
+    private final short areaCode;
+    private final short prefix;
+    private final short lineNumber;
+
+    public PhoneNumber(int areaCode, int prefix,
+                       int lineNumber) {
+        rangeCheck(areaCode,    999, "area code");
+        rangeCheck(prefix,      999, "prefix");
+        rangeCheck(lineNumber, 9999, "line number");
+        this.areaCode  = (short) areaCode;
+        this.prefix  = (short) prefix;
+        this.lineNumber = (short) lineNumber;
+    }
+
+    // helper method
+    private static void rangeCheck(int arg, int max,
+                                   String name) {
+        if (arg < 0 || arg > max)
+            throw new IllegalArgumentException(name +": " + arg);
+    }
+    @Override
+    public boolean equals(Object o) { if (o == this)
+        return true;
+        if (!(o instanceof PhoneNumber))
+            return false;
+        PhoneNumber pn = (PhoneNumber)o;
+        return pn.lineNumber == lineNumber
+                && pn.prefix  == prefix
+                && pn.areaCode  == areaCode;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneNumber{" +
+                "areaCode=" + areaCode +
+                ", prefix=" + prefix +
+                ", lineNumber=" + lineNumber +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        Map<PhoneNumber, String> m = new HashMap<PhoneNumber, String>();
+
+        // here we use *equal* but not the *same* object and get null
+//        m.put(new PhoneNumber(707, 867, 5309), "Jenny");
+//        System.out.println( m.get(new PhoneNumber(707, 867, 5309)) );
+
+        // here we use the *same* object and it works
+        PhoneNumber pn = new PhoneNumber(707, 867, 5309);
+        m.put(pn, "Jenny");
+        System.out.println( m.get(pn) );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
