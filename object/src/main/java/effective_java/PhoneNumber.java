27c2ad2d65ed96dc1f9b3d3726837527a1e72e92
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-06-20 12:01:07 +0300

    add examples of hashCode() overriding

--- object/src/main/java/effective_java/PhoneNumber.java
+++ object/src/main/java/effective_java/PhoneNumber.java
@@ -2,6 +2,7 @@ package effective_java;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by ilyarudyak on 6/20/16.
@@ -40,6 +41,13 @@ public class PhoneNumber {
     }
 
     @Override
+    // this is from Horstmann, Bloch suggests using something
+    // low-level (based on hash code for String class)
+    public int hashCode() {
+        return Objects.hash(areaCode, prefix, lineNumber);
+    }
+
+    @Override
     public String toString() {
         return "PhoneNumber{" +
                 "areaCode=" + areaCode +
@@ -48,15 +56,18 @@ public class PhoneNumber {
                 '}';
     }
 
+
     public static void main(String[] args) {
 
         Map<PhoneNumber, String> m = new HashMap<PhoneNumber, String>();
 
         // here we use *equal* but not the *same* object and get null
-//        m.put(new PhoneNumber(707, 867, 5309), "Jenny");
-//        System.out.println( m.get(new PhoneNumber(707, 867, 5309)) );
+        // if
+        m.put(new PhoneNumber(707, 867, 5309), "Jenny");
+        System.out.println( m.get(new PhoneNumber(707, 867, 5309)) );
 
         // here we use the *same* object and it works
+        // even if hashCode() is not implemented
         PhoneNumber pn = new PhoneNumber(707, 867, 5309);
         m.put(pn, "Jenny");
         System.out.println( m.get(pn) );
