ddca10e6642dab7b8b60ec6d13262bdd776be3fe
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-06-21 16:17:42 +0300

    add some reflection examples of annotations

--- annotations/src/main/java/complete_reference/Meta.java
+++ annotations/src/main/java/complete_reference/Meta.java
@@ -1,7 +1,75 @@
 package complete_reference;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Method;
+
 /**
  * Created by ilyarudyak on 6/21/16.
  */
-public class Meta {
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnno {
+    String str();
+    int val();
 }
+
+public class Meta {
+
+    @MyAnno(str = "annotation example", val = 100)
+    public static void myMeth() {
+        Meta ob = new Meta();
+
+        // Obtain the annotation for this method
+        // and display the values of the members.
+        try {
+            // First, get a Class object that represents
+            // this class.
+            Class<?> c = ob.getClass();
+
+            // Now, get a Method object that represents
+            // this method.
+            Method m = c.getMethod("myMeth");
+
+            // Next, get the annotation for this class.
+            MyAnno anno = m.getAnnotation(MyAnno.class);
+
+            // Finally, display the values.
+            System.out.println(anno);
+            System.out.println("srt='" + anno.str() + "' val=" + anno.val());
+        } catch (NoSuchMethodException exc) {
+            System.out.println("Method Not Found.");
+        }
+    }
+
+    public static void main(String[] args) {
+        myMeth();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
