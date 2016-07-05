be3d749b733b6d9dbae5373296b88c99dd834f2b
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-07-05 10:45:46 +0300

    initial commit

---
+++ mavem/simple-weather/src/test/java/weather/YahooParserTest.java
@@ -0,0 +1,14 @@
+package weather;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ilyarudyak on 7/5/16.
+ */
+public class YahooParserTest {
+
+    @org.junit.Test
+    public void testParse() throws Exception {
+
+    }
+}
---
+++ testing/src/main/java/oreilly/StringBufferDemo.java
@@ -0,0 +1,7 @@
+package oreilly;
+
+/**
+ * Created by ilyarudyak on 7/5/16.
+ */
+public class StringBufferDemo {
+}
