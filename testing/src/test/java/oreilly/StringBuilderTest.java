c8a4ef772654275600cb1da24b91ce049aec5841
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-07-05 11:15:36 +0300

    start with some simple test

---
+++ testing/src/test/java/oreilly/StringBuilderTest.java
@@ -0,0 +1,50 @@
+package oreilly;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ilyarudyak on 7/5/16.
+ */
+public class StringBuilderTest {
+
+    private StringBuilder sb;
+
+    @Before
+    public void setUp() throws Exception {
+        sb = new StringBuilder();
+
+    }
+
+    @Test @Ignore
+    public void appendTest() {
+
+        String hello = "hello, my friend!";
+        sb.append(hello);
+        assertEquals("appendTest error ...", hello, sb.toString());
+    }
+
+    @Test
+    public void sqrtTest() {
+
+        assertEquals("sqrtTest error ...", 1.414213562, Math.sqrt(2), 1E-7);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
