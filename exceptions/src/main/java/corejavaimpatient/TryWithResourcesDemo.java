596238d602463220eb1331532fbd74a7da6c5d46
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-11-25 02:04:46 +0300

    add try-with-resources demo

---
+++ exceptions/src/main/java/corejavaimpatient/TryWithResourcesDemo.java
@@ -0,0 +1,54 @@
+package corejavaimpatient;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by ilyarudyak on 11/25/16.
+ */
+public class TryWithResourcesDemo {
+
+    public static void main(String[] args) throws IOException {
+
+//        List<String> lines = Arrays.asList("Mary had a little lamb. Its fleece was white as snow.".split(" "));
+//        try (PrintWriter out = new PrintWriter("output.txt")) {
+//            for (String line : lines) {
+//                out.println(line.toLowerCase());
+//            }
+//        }
+
+        try (Scanner in = new Scanner(Paths.get("/usr/share/dict/words"));
+             PrintWriter out = new PrintWriter("output.txt")) {
+            while (in.hasNext())
+                out.println(in.next().toLowerCase());
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
